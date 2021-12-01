fcccf1776ae26a6f41d4c21a7f1b09b6677f69f6
vksakella <95061262+vksakella@users.noreply.github.com>
2021-12-01 18:49:45 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
 print("Hello Akella")
+import math
+import random
+import statistics
+
+def test_statistics():
+    """Statistics.
+    The statistics module calculates basic statistical properties (the mean, median,
+    variance, etc.) of numeric data.
+    """
+
+    data = [2.75, 1.75, 1.25, 0.25, 0.5, 1.25, 3.5]
+
+    assert statistics.mean(data) == 1.6071428571428572
+    assert statistics.median(data) == 1.25
+    assert statistics.variance(data) == 1.3720238095238095
